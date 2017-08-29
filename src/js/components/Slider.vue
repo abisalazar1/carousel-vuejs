7960d168c8f23a6a9feaa2fb11c6d104e43ddad7
Abi Salazar <abi_salazar@hotmail.co.uk>
2017-08-29 14:50:11 +0100

    timer,titles and subtitles were added

--- src/js/components/Slider.vue
+++ src/js/components/Slider.vue
@@ -3,13 +3,15 @@
 
         <div class="slideshow-container">
 
-            <div v-for="slide in slides" key="slide.index" class="Slides fade"
+            <div v-for="slide in slides" key="slide.index" class="Slides fade in"
                  :class="{ 'slideActive' : slide.isActive }">
+                <div class="sliderOverlay" v-show="overlay"></div>
                 <div class="numbertext">{{ slide.index }} / {{ slides.length}}</div>
                 <img :src="slide.img" style="width:100%">
-                <!--<div class="text">Caption Text</div>-->
+                <div class="sliderTitle">{{ slide.title }}</div>
+                <div class="sliderText">{{ slide.subtitle }}</div>
                 <a class="prev" @click="previousSlide(slide)">&#10094;</a>
-                <a class="next" @click="nextSlide(slide)">&#10095;</a>
+                <a class="next" @click="nextSlide(slide)" >&#10095;</a>
             </div>
         </div>
         <br>
@@ -21,31 +23,46 @@
 </template>
 
 <script>
-
 export default {
     props : {
           images: {
             default : () => { return [] },
             type: Array
+          },
+          timer: {
+            default : 5000,
+            type: Number
+          },
+          overlay : {
+            default: true,
+            type: Boolean
           }
          },
     data() {
         return{
-            slides : []
+            slides : [],
+            counter: 0
         }
     },
     methods : {
+        init () {
+            this.slides[this.counter].isActive = true;
+            setInterval(() => {
 
+                this.showSlide(this.slides[this.counter]);
+                if (this.counter < this.slides.length-1){
+                    this.counter++
+                } else {
+                    this.counter = 0;
+                }
+            }, this.timer);
+        },
         nextSlide (slide)  {
-
             slide.isActive = false;
-
             return (this.slides.length != slide.index)? this.slideFinder (slide.index+1) : this.slideFinder (1);
         },
         previousSlide (slide) {
-
             slide.isActive = false;
-
             return (slide.index == 1)? this.slideFinder (this.slides.length) : this.slideFinder (slide.index-1);
         },
         slideFinder (condition) {
@@ -60,31 +77,35 @@ export default {
                 index.isActive = false;
             });
             slide.isActive = true;
-        }
-
-    },
-    created () {
-        this.images.forEach((item , index) => {
-            return (index == 0 )? this.slides.push({
+        },
+        transformObject (item , index) {
+           return this.slides.push({
                 index: index+1,
-                img: item,
-                isActive: true
-            }): this.slides.push({
-                index: index+1,
-                img: item,
+                img: item.image,
+                title: item.title,
+                subtitle: item.subtitle,
                 isActive: false
             });
+        },
+        transformArray (item , index ) {
+            return this.slides.push({
+                index: index+1,
+                img: item,
+                title: '',
+                subtitle: '',
+                isActive: false
+             });
 
+        }
+    },
+    created () {
+
+        this.images.forEach((item , index) => {
+           return ( item instanceof Object)? this.transformObject(item ,index) : this.transformArray (item , index );
         });
-
-
+           this.init();
 
 
     }
 }
-
-
-
-
-
 </script>
